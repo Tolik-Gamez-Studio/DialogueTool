65830ccb4a81b0f538de6a6ce018f93a708dd90e
Lemoney <railkill@gmail.com>
2025-04-09 03:10:58 +0800

    Show preview again when preview version changes

--- common/windows/preview_window/preview_window.gd
+++ common/windows/preview_window/preview_window.gd
@@ -9,14 +9,18 @@ func _ready() -> void:
 	var is_pre_release = version.split("-").size() > 1
 	
 	var do_not_show = App.preferences.get_value("Preview", "do_not_show", false)
-	visible = is_pre_release and not do_not_show
+	var last_version = App.preferences.get_value("Preview", "last_version", "")
+	var is_new = version != last_version
+	visible = is_pre_release and (not do_not_show or is_new)
 	grab_focus()
 
 
 func _on_button_pressed() -> void:
 	if dns_checkbox:
 		var checked = dns_checkbox.button_pressed
+		var version = ProjectSettings.get("application/config/version")
 		App.preferences.set_value("Preview", "do_not_show", checked)
+		App.preferences.set_value("Preview", "last_version", version)
 		App.preferences.save(Constants.PREFERENCES_PATH)
 	hide()
 
