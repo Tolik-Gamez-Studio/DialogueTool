eb2ac8e6b399696b8a41bdcf5cf30b6bd40a8a08
Lemoney <railkill@gmail.com>
2025-04-09 01:45:24 +0800

    Add do-not-show checkbox for preview window

--- common/windows/preview_window/preview_window.gd
+++ common/windows/preview_window/preview_window.gd
@@ -1,14 +1,29 @@
 extends Window
 
 
+@export var dns_checkbox: CheckBox
+@export var preferences_path: String = "user://preferences.save"
+
+var preferences: ConfigFile = ConfigFile.new()
+
+
 func _ready() -> void:
+	var error = preferences.load(preferences_path)
+	var do_not_show: bool
+	if error == OK:
+		do_not_show = preferences.get_value("Preview", "do_not_show", false)
+	
 	var version = ProjectSettings.get("application/config/version")
 	var is_pre_release = version.split("-").size() > 1
 	
-	visible = is_pre_release
+	visible = is_pre_release and not do_not_show
 	grab_focus()
 
+
 func _on_button_pressed() -> void:
+	if dns_checkbox:
+		preferences.set_value("Preview", "do_not_show", dns_checkbox.button_pressed)
+		preferences.save(preferences_path)
 	hide()
 
 
