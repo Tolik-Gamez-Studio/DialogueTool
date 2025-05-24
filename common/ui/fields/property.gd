623ec601bd3d4bf0bd3c88b47a4ecd49e7cee4e0
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2025-05-24 16:34:14 +0200

    Fix renaming a character reset it
    When the value of a property of type Dictionary is changed, it will be merged with the previous value to avoid losing any data.

--- common/ui/fields/property.gd
+++ common/ui/fields/property.gd
@@ -75,6 +75,9 @@ func propagate(new_value: Variant, can_display: bool = true) -> void:
 
 ## Trigger a property value change only when valeus are different.
 func save_value(new_value: Variant) -> void:
+	if new_value is Dictionary:
+		new_value = value.merged(new_value)
+	
 	if not Util.is_equal(value, new_value):
 		change.emit(value, new_value)
 
