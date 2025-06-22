ae64d5ae1aa1d91d4f1fe7c6820d8f9e3b9a2e72
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2025-06-22 02:44:24 +0200

    Searchbar mockup

--- common/layouts/search_bar/search_bar.gd
+++ common/layouts/search_bar/search_bar.gd
@@ -1,13 +1,17 @@
 extends PanelContainer
 
+const SCROLL_CONTAINER_MAX_SIZE: int = 200
+
 @export var graph_edit_switcher: GraphEditSwitcher
 
-@onready var line_edit: LineEdit = $HBoxContainer/LineEdit
+@onready var line_edit: LineEdit = %LineEdit
 
 
 func focus() -> void:
 	line_edit.grab_focus()
 	line_edit.select_all()
+	
+	_on_h_box_resized()
 
 
 func _on_line_edit_text_changed(new_text: String) -> void:
@@ -17,3 +21,7 @@ func _on_line_edit_text_changed(new_text: String) -> void:
 	for node: MonologueGraphNode in all_nodes:
 		if node.node_type.containsn(new_text):
 			continue
+
+
+func _on_h_box_resized() -> void:
+	%ScrollContainer.custom_minimum_size.y = min(%HintVBox.size.y, SCROLL_CONTAINER_MAX_SIZE)
